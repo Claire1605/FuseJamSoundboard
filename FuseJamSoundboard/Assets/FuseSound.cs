db4b122ecb47201abd88e55664d83476e984e4f7
Claire1605 <claire1605@gmail.com>
2018-07-06 18:24:52 +0100

    sound board

---
+++ FuseJamSoundboard/Assets/FuseSound.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(AudioSource))]
+public class FuseSound : MonoBehaviour {
+   
+    public List<Button> buttons = new List<Button>();
+    public List<KeyCode> keycodes = new List<KeyCode>();
+
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        for (int x = 0; x < keycodes.Count; x++)
+        {
+            if (Input.GetKeyDown(keycodes[x]))
+            {
+                buttons[x].Select();
+                buttons[x].onClick.Invoke();
+                Invoke("DeselectButton", 0.5f);
+            }
+        }
+    }
+
+    private void DeselectButton()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+    }
+
+    public void fusionTimeLetsGo(AudioClip sound)
+    {
+        if (!audioSource.isPlaying)
+            audioSource.PlayOneShot(sound);
+    }
+}
---
+++ FuseJamSoundboard/Assets/JammyText.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Outline))]
+public class JammyText : MonoBehaviour {
+
+    private Outline outline;
+    public int xMult = 25;
+    public int yMult = 50;
+
+    private void Start()
+    {
+        outline = GetComponent<Outline>();
+    }
+
+    private void Update()
+    {
+        float a = Mathf.Sin(Time.time);
+        float b = Mathf.Cos(Time.time);
+        outline.effectDistance = new Vector2(a * xMult, b * yMult);
+    }
+}
